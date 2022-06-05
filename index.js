34d280ce89847d3d7c83ec7f62edbf38420f0f65
Kagamins <nsoulper@gmail.com>
2022-06-05 09:19:52 +0300

    Update index.js

--- index.js
+++ index.js
@@ -7,3 +7,18 @@ if("serviceWorker" in  navigator){
         console.log(error);
     });
 }
+
+     function addData(){
+     document.getElementById("tablebody").SetInnerHtml("<tr>
+        <td><div class="form-check"><input class="form-check-input" type="checkbox" value="" id="seventhperiod"></div></td>
+        <td><div class="form-check"><input class="form-check-input" type="checkbox" value="" id="sixthperiod"></div></td>
+        <td><div class="form-check"><input class="form-check-input" type="checkbox" value="" id="fithperiod"></div></td>
+        <td><div class="form-check"><input class="form-check-input" type="checkbox" value="" id="fourthperiod"></div></td>
+        <td><div class="form-check"><input class="form-check-input" type="checkbox" value="" id="thirdperiod"></div></td>
+        <td><div class="form-check"><input class="form-check-input" type="checkbox" value="" id="secondperiod"></div></td>
+        <td><div class="form-check"><input class="form-check-input" type="checkbox" value="" id="firstperiod"></div></td>
+        <td></td>
+        <td>١١د١</td>
+        <td>dummy</td>
+      </tr>")
+     }
